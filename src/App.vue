58b95bab7b01a0fe56af74852cc1c403229c676c
yudachao <290945266@qq.com>
2019-11-21 15:51:20 +0800

    feat(all): 初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+// 禁止手机端扩大和缩小手势
+window.onload = () => {
+  document.addEventListener("touchstart", (event: any) => {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  });
+  document.addEventListener("gesturestart", (event: any) => {
+    event.preventDefault();
+  });
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import router from "./router";
+
+import "@/styles/normalize.scss";
+import "@/styles/reset.scss";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App)
+}).$mount("#app");
